0e321896a048b098c4c9d77c6d82a5f45bd11bc1
Livenga <ljmmz0405@gmail.com>
2017-07-02 17:48:00 +0900

    include files

---
+++ include/common.h
@@ -0,0 +1,7 @@
+#ifndef _COMMON_H
+#define _COMMON_H
+
+#define MHz         000000
+#define _XTAL_FREQ 4MHz
+
+#endif
---
+++ include/init.h
@@ -0,0 +1,15 @@
+#ifndef _INIT_H
+#define _INIT_H
+
+
+#pragma config FOSC  = XT
+#pragma config MCLRE = OFF
+#pragma config PWRTE = ON
+#pragma config WDTE  = OFF
+#pragma config BOREN = OFF
+#pragma config LVP   = OFF
+
+#pragma config CPD   = OFF
+#pragma config CP    = OFF
+
+#endif
